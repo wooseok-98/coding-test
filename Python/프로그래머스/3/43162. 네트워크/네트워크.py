25ddc475f89adb8c97bf8f36ff658eda01e11fb2
최우석 <9872dan57@gmail.com>
2026-09-05 11:11:56 +0900

    [level 3] Title: 네트워크, Time: 0.26 ms, Memory: 11.6 MB -BaekjoonHub

--- Python/프로그래머스/3/43162. 네트워크/네트워크.py
+++ Python/프로그래머스/3/43162. 네트워크/네트워크.py
@@ -2,19 +2,18 @@ from collections import deque
 
 def solution(n, computers):
     
-    visited = [False]*n
+    visited = [False] * n
     
     def bfs(start):
         dq = deque([start])
         visited[start] = True
         while dq:
             cur = dq.popleft()
-            for nxt in range(n):
-                if computers[cur][nxt] == 0: continue
-                if visited[nxt]: continue
-                visited[nxt] = True
-                dq.append(nxt)
-                
+            for nx in range(n):
+                if computers[cur][nx] == 0: continue
+                if visited[nx]: continue
+                visited[nx] = True
+                dq.append(nx)
     count = 0
     for i in range(n):
         if not visited[i]:
