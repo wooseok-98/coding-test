cc594951407d8c657e3579fbb41c4fbf9538e395
최우석 <9872dan57@gmail.com>
2026-09-16 21:47:20 +0900

    [level 2] Title: 귤 고르기, Time: 25.61 ms, Memory: 22.6 MB -BaekjoonHub

--- Python/프로그래머스/2/138476. 귤 고르기/귤 고르기.py
+++ Python/프로그래머스/2/138476. 귤 고르기/귤 고르기.py
@@ -3,19 +3,12 @@ from collections import Counter
 def solution(k, tangerine):
     answer = 0
     
-    size = Counter()
-    for t in tangerine:
-        size[t] = size.get(t, 0) + 1
+    size = Counter(tangerine)
     
     for _, v in size.most_common():
-        if v >= k:
-            answer += 1
+        answer += 1
+        k -= v
+        if k <= 0:
             break
-        else:
-            k = k - v
-            if k > 0:
-                answer += 1
-            else:
-                break
             
     return answer
