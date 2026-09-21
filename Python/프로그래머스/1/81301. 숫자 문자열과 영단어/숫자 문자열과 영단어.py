ea39c4630916f84ed8c3f325548944ad0d1523bd
최우석 <9872dan57@gmail.com>
2026-09-21 21:46:44 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.01 ms, Memory: 11.1 MB -BaekjoonHub

--- Python/프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
+++ Python/프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -1,16 +1,9 @@
 def solution(s):
-    answer = 0
+
+    words = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
     
-    s = s.replace("zero", "0")
-    s = s.replace("one", "1")
-    s = s.replace("two", "2")
-    s = s.replace("three", "3")
-    s = s.replace("four", "4")
-    s = s.replace("five", "5")
-    s = s.replace("six", "6")
-    s = s.replace("seven", "7")
-    s = s.replace("eight", "8")
-    s = s.replace("nine", "9")
+    for i, w in enumerate(words):
+        s = s.replace(w, str(i))
+        
     answer = int(s)
-    
     return answer
