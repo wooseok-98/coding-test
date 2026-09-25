c72566e320f10bf2bd0ff2bfebffb02d4c5ab2ab
최우석 <9872dan57@gmail.com>
2026-09-25 17:04:06 +0900

    [level 2] Title: 할인 행사, Time: 125.37 ms, Memory: 18.5 MB -BaekjoonHub

---
+++ Python/프로그래머스/2/131127. 할인 행사/할인 행사.py
@@ -0,0 +1,16 @@
+from collections import Counter
+
+def solution(want, number, discount):
+    answer = 0
+    
+    # 구매 항목+수량
+    shop = Counter()
+    for i, w in enumerate(want):
+        shop[w] = number[i]
+    
+    for i in range(len(discount)-9):
+        day_discount = Counter(discount[i:i+10])
+        if shop == day_discount:
+            answer += 1
+    
+    return answer
